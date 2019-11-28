b70c7aba7b1df1336b8c741079c784bbe81d8c27
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-28 09:12:14 -0700

    finished first draft, rewriting for practice

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function getFirstName() {
+  console.log(document.getElementById("name-id").value);
+  document
+    .getElementById("button")
+    .addEventListener("click", event => greeting(event));
+}
+
+getFirstName();
+
+function greeting(event) {
+  event.preventDefault();
+  let firstName = document.getElementById("name-id").value.split(" ")[0];
+  document.getElementsByClassName("results")[0].textContent = firstName;
+  console.log(`It's so nice to meet you, ${firstName}`);
+}
